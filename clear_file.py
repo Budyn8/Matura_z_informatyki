cbee519e44e532d6978c18e7026118dce668af86
Budyn <kilmur019@gmail.com>
2023-09-25 23:23:54 +0200

    adding an env files

--- clear_file.py
+++ clear_file.py
@@ -1,1 +1,10 @@
-print("Czysty plik")
+def main():
+    print("Czysty plik")
+
+    print("looking at my setup")
+
+    print("making shure everything is good")
+
+
+if __name__ == '__main__':
+    main()
