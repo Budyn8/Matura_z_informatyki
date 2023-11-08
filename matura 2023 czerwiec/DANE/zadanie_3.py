4cb45b2c1e41d90900204af4bbba2182fa7fc58a
Budzyński <m.budzyski.2019@t9l.local>
2023-11-08 14:20:35 +0100

    przyklad 2

--- matura 2023 czerwiec/DANE/zadanie_3.py
+++ matura 2023 czerwiec/DANE/zadanie_3.py
@@ -1,31 +1,51 @@
-def podpunkt1():
+def podpunkt_1(file_lines):
 
-    dane = open("./DANE_E/anagram.txt", 'r')
-    text = dane.read()
+    balanced = [0, 0]
 
-    linije = text.split('\n')
-    zrownowarzona = [0, 0]
-
-    for linija in linije:
-        if linija == '':
+    for line in file_lines:
+        if line == '':
             continue
         count = [0, 0]
 
-        for num in linija:
+        for num in line:
             count[int(num)] += 1
 
-        rownowaga = count[0] - count[1]
+        balance = count[0] - count[1]
 
-        if rownowaga < 0:
-            rownowaga *= -1
+        if balance < 0:
+            balance *= -1
 
-        if rownowaga < 2:
-            zrownowarzona[rownowaga] += 1
+        if balance < 2:
+            balanced[balance] += 1
 
-    return zrownowarzona
+    return balanced
+
+def podpunkt_2(file_content):
+
+    liczby = []
+    max_lenght = 0
+
+    for line in file_content:
+
+        lenght = len(line)
+
+        if lenght > max_lenght:
+            max_lenght = lenght
+            liczby = [line]
+            continue
+
+        liczby += line
+
+    return 0
 
 
 if __name__ == '__main__':
-    wynikf = open('./wynik3.txt', 'w')
-    wynik = podpunkt1()
-    wynikf.write(str(wynik[0]) + " " + str(wynik[1]))
+
+    dane = open("./DANE_E/anagram.txt", 'r')
+
+    if dane:
+        text = dane.read()
+        text = text.split('\n')
+        wynikf = open('./wynik3.txt', 'w')
+        wynik = podpunkt_1(text)
+        wynikf.write(str(wynik[0]) + " " + str(wynik[1]))
