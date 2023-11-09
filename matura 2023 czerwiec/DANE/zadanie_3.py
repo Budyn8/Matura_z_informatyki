d266f6cde2da34e4ee5b3ae5b0d9873d44d29355
Budzyński <m.budzyski.2019@t9l.local>
2023-11-09 13:21:33 +0100

    przyklad 2

--- matura 2023 czerwiec/DANE/zadanie_3.py
+++ matura 2023 czerwiec/DANE/zadanie_3.py
@@ -1,3 +1,17 @@
+def check_balance(line):
+    count = [0, 0]
+
+    for num in line:
+        count[int(num)] += 1
+
+    balance = count[0] - count[1]
+
+    if balance < 0:
+        balance *= -1
+
+    return balance
+
+
 def podpunkt_1(file_lines):
 
     balanced = [0, 0]
@@ -5,20 +19,14 @@ def podpunkt_1(file_lines):
     for line in file_lines:
         if line == '':
             continue
-        count = [0, 0]
 
-        for num in line:
-            count[int(num)] += 1
-
-        balance = count[0] - count[1]
-
-        if balance < 0:
-            balance *= -1
+        balance = check_balance(line)
 
         if balance < 2:
             balanced[balance] += 1
 
     return balanced
+
 
 def podpunkt_2(file_content):
 
@@ -29,23 +37,23 @@ def podpunkt_2(file_content):
 
         lenght = len(line)
 
-        if lenght > max_lenght:
-            max_lenght = lenght
-            liczby = [line]
-            continue
+        if lenght == 8 and check_balance(line) < 2:
+            liczby.append(line)
 
-        liczby += line
-
-    return 0
+    return liczby
 
 
 if __name__ == '__main__':
 
-    dane = open("./DANE_E/anagram.txt", 'r')
+    dane = open("./DANE_E/przyklad.txt", 'r')
 
     if dane:
         text = dane.read()
         text = text.split('\n')
+
         wynikf = open('./wynik3.txt', 'w')
-        wynik = podpunkt_1(text)
-        wynikf.write(str(wynik[0]) + " " + str(wynik[1]))
+        wynik_1 = podpunkt_1(text)
+
+        wynik_2 = podpunkt_2(text)
+
+        # wynikf.write(str(wynik[0]) + " " + str(wynik[1]))
